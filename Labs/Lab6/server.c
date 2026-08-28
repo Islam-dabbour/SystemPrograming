acd93e46d44809e1c72e85250b92d4e225eb8413
Islam Dabbour <islamdabbour25@gmail.com>
2026-08-29 01:08:09 +0300

    Adding Lab 6 files

--- Labs/Lab6/server.c
+++ Labs/Lab6/server.c
@@ -4,82 +4,35 @@
 #include <unistd.h>
 #include <arpa/inet.h>
 #include <pthread.h>
-#include <time.h>
+
+
 
 void error(const char *msg){
 perror(msg); 
 exit(0); 
 }
 
-struct client_arg { int sock; };
+int main(int argc, char **argv){
 
-void *client_handler(void *arg) {
-  struct client_arg *c = arg;
-  int client_sock = c->sock;
-  free(c);
-
-  char buffer[1024];
-  int n;
-  int targetNumber = (rand() % 100) + 1;
-  int numberOfAttempts = 0;
-
-  /* optional read of initial client message */
-  bzero(buffer, 1024);
-  n = read(client_sock, buffer, 1023);
-  if (n > 0) {
-    buffer[n] = '\0';
-    printf("From Client: %s\n", buffer);
-  }
-
-  strcpy(buffer, "HI, THIS IS SERVER. HAVE A NICE DAY!!!");
-  write(client_sock, buffer, strlen(buffer) + 1);
-
-  while (1) {
-    bzero(buffer, 1024);
-    n = read(client_sock, buffer, 1023);
-    if (n <= 0) break;
-    buffer[n] = '\0';
-
-    int guess = atoi(buffer);
-    numberOfAttempts++;
-
-    if (guess < targetNumber) {
-      strcpy(buffer, "Your guess is too low. Try again.");
-      printf("%d\n", guess);
-      write(client_sock, buffer, strlen(buffer) + 1);
-    } else if (guess > targetNumber) {
-      strcpy(buffer, "Your guess is too high. Try again.");
-      write(client_sock, buffer, strlen(buffer) + 1);
-    } else {
-      bzero(buffer, 1024);
-      sprintf(buffer, "Congratulations! You've guessed the number %d in %d attempts.", targetNumber, numberOfAttempts);
-      write(client_sock, buffer, strlen(buffer) + 1);
-      break;
-    }
-  }
-
-  close(client_sock);
-  printf("[-]Client disconnected.\n");
-  return NULL;
-}
-
-int main(int argc, char **argv) {
-  if (argc != 2) {
+  //char *ip = "127.0.0.1"; //// this is used if you need to connect to a specific IP which should be passed to inet_addr(ip) 
+  if(argc != 2){
     printf("You need to provide a port number in the program argument\n");
     exit(0);
   }
 
-  srand(time(NULL));
+  int targetNumber = (rand() % 100) + 1;
+  int numberOfAttempts = 0;
 
   int port = atoi(argv[1]);
   printf("Port: %d\n", port);
 
   int server_sock, client_sock;
   struct sockaddr_in server_addr, client_addr;
-  int addr_size, n;
+  int addr_size,n;
+  char buffer[1024];
 
   server_sock = socket(AF_INET, SOCK_STREAM, 0);
-  if (server_sock < 0) {
+  if (server_sock < 0){
     perror("[-] ERROR opening socket");
     exit(1);
   }
@@ -88,42 +41,67 @@ int main(int argc, char **argv) {
   memset(&server_addr, '\0', sizeof(server_addr));
   server_addr.sin_family = AF_INET;
   server_addr.sin_port = htons(port);
-  server_addr.sin_addr.s_addr = INADDR_ANY;
+  server_addr.sin_addr.s_addr = INADDR_ANY  ;//inet_addr(ip); ; 
 
   n = bind(server_sock, (struct sockaddr*)&server_addr, sizeof(server_addr));
-  if (n < 0) {
+  if (n < 0){
     perror("[-]Bind error");
     exit(1);
   }
   printf("[+]Bind to the port number: %d\n", port);
 
-  listen(server_sock, 5);
-  while (1) {
+    listen(server_sock, 5);
+    while (1){ // if we need to keep the server always running
     printf("Listening...\n");
     addr_size = sizeof(client_addr);
     client_sock = accept(server_sock, (struct sockaddr*)&client_addr, &addr_size);
-    if (client_sock < 0) error("ERROR on accept");
+    if (client_sock < 0) 
+      error("ERROR on accept");
+
 
     printf("[+]Client connected.\n");
 
-    struct client_arg *c = malloc(sizeof(*c));
-    if (!c) {
-      perror("malloc");
-      close(client_sock);
-      continue;
-    }
-    c->sock = client_sock;
+    bzero(buffer, 1024);
+    read(client_sock,buffer,1024  );
+    printf("From Client: %s\n", buffer);
 
-    pthread_t tid;
-    if (pthread_create(&tid, NULL, client_handler, c) != 0) {
-      perror("pthread_create");
-      close(client_sock);
-      free(c);
-      continue;
+    bzero(buffer, 1024);
+    strcpy(buffer, "HI, THIS IS SERVER. HAVE A NICE DAY!!!");
+    write(client_sock,buffer,strlen(buffer));
+
+    
+    int guess = -1;
+  while (1) {
+    bzero(buffer, 1024);
+    n = read(client_sock, buffer, 1024);
+    
+    guess = atoi(buffer);
+    numberOfAttempts++;
+
+    if (guess < targetNumber) {
+      bzero(buffer, 1024);
+      strcpy(buffer, "Your guess is too low. Try again.");
+      printf("%d \n", guess);
+      write(client_sock, buffer, strlen(buffer) + 1);
+    } else if (guess > targetNumber) {
+      bzero(buffer, 1024);
+      strcpy(buffer, "Your guess is too high. Try again.");
+      write(client_sock, buffer, strlen(buffer) + 1);
+    } else {
+      bzero(buffer, 1024);
+      sprintf(buffer, "Congratulations! You've guessed the number %d in %d attempts.", targetNumber, numberOfAttempts);
+      write(client_sock, buffer, strlen(buffer) + 1);
+      
+  close(server_sock);
+      break;
     }
-    pthread_detach(tid);
   }
 
+    
+    close(client_sock);
+    printf("[-]Client disconnected.\n\n");
+} //end of while for always sever running
+ 
   close(server_sock);
   return 0;
 }
